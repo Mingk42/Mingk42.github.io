56a53014c2443642b148dcf656b5c6657f2a7121
Mingk42 <xoals123456t@gmail.com>
2024-07-11 10:16:48 +0900

    기능 정리

--- sudden.sh
+++ sudden.sh
@@ -4,15 +4,12 @@ FILE=~/tmp/_SUDDEN
 
 while true
 do
-
-	echo "check $FILE"
 #        if -e "$FILE"; then                  # 괄호가 필수인지 궁금해서 안 쓰고 해봤는데 -e:not found 오류가 나타났다.
 #        if [-e "$FILE"]; then                # 괄호를 적었는데 다시 [-e:not found 오류가 나타났다. 괄호&공백이 필수인 것으로 확인
-        if [ -f "$FILE" ]; then               # 정상적으로 동작!!!
+        if [ -f "$FILE" ]; then               # 정상적으로 동작!!! e:단순히 파일이 있는지, f: 파일이 있고 일반파일인지
 		echo "SUDDEN!!!"	
 	else
-		clear
+		date
 	fi
-	date
 	sleep 3
 done
